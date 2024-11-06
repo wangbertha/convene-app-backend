3b85d4b0877f9ad7fe26da185977f1004e068820
Edwin <edwincastaneda0801@gmail.com>
2024-11-06 11:24:42 -0600

    Chat first functionality working

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const { createServer } = require("http");
-const socketIo = require("socket.io");
+const { Server } = require("socket.io");
 require("dotenv").config();
 
 const app = express();
 const server = createServer(app);
-const io = socketIo(server); // Initialize socket.io with the HTTP server
+const PORT = 3000;
 
-const PORT = process.env.PORT || 3000;
+// Initialize socket.io
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  },
+});
 
 // cors
 const cors = require("cors");
@@ -34,6 +42,17 @@ app.use((err, req, res, next) => {
 // Socket.io connection handling
 io.on("connection", (socket) => {
   console.log("New client connected");
+
+  // Join a room
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`Client joined room: ${room}`);
+  });
+
+  // Send message to a room
+  socket.on("send_message", ({ room, message, timestamp, sender }) => {
+    io.to(room).emit("receive_message", { message, timestamp, sender });
+  });
 
   socket.on("disconnect", () => {
     console.log("Client disconnected");
